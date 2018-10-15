53f4ba0ea72e1b94265b161e87a5adca3a7b5931
georgesykes@UKML70AEJG5J <george.sykes@tesco.com>
2018-10-15 20:32:13 +0100

    Add callback to app.py to enable update of graph from drop-down

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import dash, datetime, math, os, sys, argparse
+from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.plotly as py
@@ -31,43 +32,6 @@ df = dp.parse_dates(df)
 
 mapbox_access_token = os.environ.get('MAPBOX_ACCESS_TOKEN')
 
-data = [
-    go.Scattermapbox(
-        lat=df['lat'],
-        lon=df['lon'],
-        mode='markers',
-        marker=dict(
-            size=9,
-            color=dp.map_percentiles(df["age"].values),
-            colorscale=[[0, 'rgb(244,236,21)'], [0.021, 'rgb(218,240,23)'],
-                                [0.042, 'rgb(187,236,25)'], [0.063, 'rgb(157,232,27)'],
-                                [0.084, 'rgb(128,228,29)'], [0.115, 'rgb(102,224,31)'],
-                                [0.146, 'rgb(76,220,32)'], [0.170, 'rgb(52,216,34)'],
-                                [0.225, 'rgb(36,210,73)'], [0.275, 'rgb(37,208,66)'],
-                                [0.325, 'rgb(38,204,88)'], [0.375, 'rgb(40,200,109)'],
-                                [0.4, 'rgb(41,196,129)'], [0.5, 'rgb(42,192,147)'],
-                                [0.6, 'rgb(43,188,164)'],[1.0, 'rgb(97,48,153)']]
-        ),
-    )
-]
-
-layout = go.Layout(
-    autosize=True,
-    hovermode='closest',
-    height=700,
-    mapbox=dict(
-        accesstoken=mapbox_access_token,
-        bearing=0,
-        center=dict(
-            lat=df.iloc[0].lat,
-            lon=df.iloc[0].lon
-        ),
-        pitch=0,
-        zoom=10,
-        style="dark"
-    ),
-)
-
 app.layout = html.Div(children=[
     html.H1(children='Property Dash'),
 
@@ -83,18 +47,58 @@ app.layout = html.Div(children=[
                     {'label': 'No. of bedrooms', 'value': 'bedrooms'},
                     {'label': 'Property type', 'value': 'property_subtype'}
                 ],
-                value="Price",
+                value="price",
                 placeholder="Price",
                 className="feature-picker"
             ),
     dcc.Graph(
-        id='prop_details',
-        figure={
-            'data': data,
-            'layout': layout
-        }
+        id='map-graph',
     )
 ])
+
+@app.callback(Output("map-graph", "figure"),
+              [Input("feature-dropdown", "value")])
+def update_map(value):
+    data = [
+        go.Scattermapbox(
+            lat=df['lat'],
+            lon=df['lon'],
+            mode='markers',
+            marker=dict(
+                size=9,
+                color=dp.map_percentiles(df[value].values),
+                colorscale=[[0, 'rgb(244,236,21)'], [0.021, 'rgb(218,240,23)'],
+                                    [0.042, 'rgb(187,236,25)'], [0.063, 'rgb(157,232,27)'],
+                                    [0.084, 'rgb(128,228,29)'], [0.115, 'rgb(102,224,31)'],
+                                    [0.146, 'rgb(76,220,32)'], [0.170, 'rgb(52,216,34)'],
+                                    [0.225, 'rgb(36,210,73)'], [0.275, 'rgb(37,208,66)'],
+                                    [0.325, 'rgb(38,204,88)'], [0.375, 'rgb(40,200,109)'],
+                                    [0.4, 'rgb(41,196,129)'], [0.5, 'rgb(42,192,147)'],
+                                    [0.6, 'rgb(43,188,164)'],[1.0, 'rgb(97,48,153)']]
+            ),
+        )
+    ]
+
+    layout = go.Layout(
+        autosize=True,
+        hovermode='closest',
+        height=700,
+        mapbox=dict(
+            accesstoken=mapbox_access_token,
+            bearing=0,
+            center=dict(
+                lat=df.iloc[0].lat,
+                lon=df.iloc[0].lon
+            ),
+            pitch=0,
+            zoom=10,
+            style="dark"
+        ),
+    )
+
+    return go.Figure(data=data, layout=layout)
+
+
 
 if __name__ == '__main__':
     app.run_server()
