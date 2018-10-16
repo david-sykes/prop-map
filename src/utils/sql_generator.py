10a88ab3d9733d69fe7d014e495c281c868ec2a8
georgesykes@UKML70AEJG5J <george.sykes@tesco.com>
2018-10-16 22:23:06 +0100

    Fix bug in sql generator

--- src/utils/sql_generator.py
+++ src/utils/sql_generator.py
@@ -1,8 +1,8 @@
 class SqlGenerator(object):
     QUERIES = {
         "ALL": """SELECT * FROM properties""",
-        "SUBSET": """SELECT * FROM properties WHERE retrieved_at = '2018-10-13' and search_name like 'N%'"""
-        "LARGE"; """SELECT * FROM properties WHERE retrieved_at >= '2018-10-14' and retrieved_at < '2018-10-16'"""
+        "SUBSET": """SELECT * FROM properties WHERE retrieved_at = '2018-10-13' and search_name like 'N%'""",
+        "LARGE": """SELECT * FROM properties WHERE retrieved_at >= '2018-10-14' and retrieved_at < '2018-10-16'"""
         }
 
     DEFAULT = "Not a valid query category"
